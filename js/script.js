4f9a860cdb5a6e4ad6b87e74d477da8ee47ba6a0
Shakhrizade <125201941+Shakhrizade@users.noreply.github.com>
2023-06-25 09:36:03 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var name = prompt("Ismingizni kiriting")
+console.log("Mening ismim " + name);
+
+var age = prompt("Yoshingizni kiriting")
+console.log("Men "+ age +" Yoshdaman ");
+
+var num = +prompt("Misol 1: 6+3=?")
+console.log("Misol 1: 6+3= "+(6+3)+ " Sizning jovobingiz " +num);
+
+var num = +prompt("Misol 2: 15-10=?")
+console.log("Misol 2: 15-10= "+(15-10)+" Sizning jovobingiz " + num);
+
+var num = +prompt("Misol 3: 63%12=?")
+console.log("Misol 3: 63%12= "+(63%12)+" Sizning jovobingiz " + num);
+
+var num = +prompt("Misol 4: 645/5=?")
+console.log("645/5= "+(645/5)+" Sizning jovobingiz " + num);
+
+var num = +prompt("Misol 5: 19*9=?")
+console.log("Misol 5: 19*9= "+(19*9)+" Sizning jovobingiz " + num);
+
+var num = +prompt("Misol 6: 462%43=?")
+console.log("Misol 6: 462%43= "+(462%43)+" Sizning jovobingiz " + num);
+
+var num = +prompt("Misol 7: 43+7853=?")
+console.log("Misol 7: 43+7853=" +(43+7853)+" Sizning jovobingiz " + num);
+
+var num = +prompt("Misol 8: 544-78=?")
+console.log("Misol 8: 544-78= "+(544-78)+" Sizning jovobingiz " + num);
+
+var num = +prompt("Misol 9: 119*91=?")
+console.log("Misol 9: 119*91= "+(119*91)+" Sizning jovobingiz " + num);
+
+alert("Natijani ko'rish uchun konsolni bosing")
